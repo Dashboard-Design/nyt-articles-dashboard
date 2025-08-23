d39c38bef68dbefda743785352a8f7de47b4141b
Sajjad Ahmadi <sajadahmadi727@gmail.com>
2025-08-23 19:35:20 +0330

    adding line chart and cards

--- components/search_section.py
+++ components/search_section.py
@@ -1,3 +1,35 @@
+# import dash_bootstrap_components as dbc
+# from dash import html
+
+# def create_search_section():
+#     return dbc.Container(
+#     [
+#         html.H4("Explore Article Trends", className="text-center"),
+#         html.P(
+#             "Search for keywords to analyze article publication trends over the last 15 years",
+#             className="mt-2 text-center text-muted fs-6",
+#         ),
+#         dbc.Row(
+#             [
+#                 dbc.Col(
+#                     dbc.Input(
+#                         type="text",
+#                         placeholder="Enter keywords to analyze (e.g., climate change, technology, politics)",
+#                     ),
+#                     width=9,
+#                 ),
+#                 dbc.Col(
+#                     dbc.Button("Analyze", color="secondary", className="w-100"),
+#                     width=2,
+#                 ),
+#             ],
+#             justify="center",
+#             className="mt-3  fs-6",
+#         ),
+#     ],
+#     className="my-5 bg-white p-4 rounded-3"
+#     )
+
 import dash_bootstrap_components as dbc
 from dash import html
 
@@ -13,13 +45,19 @@ def create_search_section():
             [
                 dbc.Col(
                     dbc.Input(
+                        id="keyword-input",
                         type="text",
                         placeholder="Enter keywords to analyze (e.g., climate change, technology, politics)",
                     ),
                     width=9,
                 ),
                 dbc.Col(
-                    dbc.Button("Analyze", color="secondary", className="w-100"),
+                    dbc.Button(
+                        "Analyze", 
+                        id="search-button",
+                        color="secondary", 
+                        className="w-100"
+                    ),
                     width=2,
                 ),
             ],
@@ -27,5 +65,5 @@ def create_search_section():
             className="mt-3  fs-6",
         ),
     ],
-    className="my-5 bg-white p-4 rounded-3"
+    className="shadow-sm my-5 bg-white p-4 rounded-3"
     )
